ec7715064d17e48ba7e4cd8cd62e3f42f9c591cd
Aditya DS <adithya18062000@gmail.com>
2021-02-21 22:00:25 +0530

    removed question

--- Assignment-1/Q4.py
+++ Assignment-1/Q4.py
@@ -1,6 +1,3 @@
-# Download Lena image and scale it by factors of 1, 2, 0.5 using bilinear interpolation and display the scaled images.
-# Also, display the output of built-in functions for doing scaling by factors of 0.5, 2. Compare the results.
-
 import cv2 as cv
 import numpy as np
 from math import floor
