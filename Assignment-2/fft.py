220a9a43c026ea85f437a9155242df14ef93ce7d
Aditya DS <adithya18062000@gmail.com>
2021-04-09 22:48:23 +0530

    1D FFT function

---
+++ Assignment-2/fft.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def fft(x):
+    x = np.asfarray(x)
+    N = x.shape[0]
+    twiddle = np.exp(-2j*np.pi/N)
+    if N % 2 > 0:
+        raise ValueError("must be a power of 2")
+    elif N <= 2:
+        n = np.arange(N)
+        k = n.reshape((N, 1))
+        return np.dot(twiddle**(n*k), x)
+    else:
+        X_even = fft(x[::2])
+        X_odd = fft(x[1::2])
+        terms = twiddle**np.arange(N)
+        return np.concatenate([X_even + terms[:int(N/2)] * X_odd, X_even + terms[int(N/2):] * X_odd])
