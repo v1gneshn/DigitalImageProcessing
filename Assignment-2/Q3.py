234f5c10ffeda65e2b066f27f5e35aac11d22a8c
Aditya DS <adithya18062000@gmail.com>
2021-04-10 02:36:22 +0530

    refactor

--- Assignment-2/Q3.py
+++ Assignment-2/Q3.py
@@ -3,6 +3,10 @@ from fft import fft
 
 
 x = [1, 2, 3, 4, 5, 6, 7, 8]
-print(fft(x))
-print(np.fft.fft(x))
-print(np.allclose(fft(x), np.fft.fft(x)))
+
+npFFT = np.fft.fft(x)
+myFFT = fft(x)
+
+print(myFFT)
+print(npFFT)
+print(np.allclose(myFFT, npFFT))
