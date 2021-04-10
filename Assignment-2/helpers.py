5ff9fe18934acc05eac353acd4af8457c27e69fd
Aditya DS <adithya18062000@gmail.com>
2021-04-10 12:59:36 +0530

    helper function to plot spectrum

---
+++ Assignment-2/helpers.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2 as cv
+
+
+def plotSpectrum(fft, fileName, magOnly=False):
+    fft = np.fft.fftshift(fft)
+
+    mag = np.absolute(fft)
+    mag = 255*(mag/np.amax(mag))
+    mag = 10*np.log10(1+mag)
+    cv.namedWindow(f'{fileName} (Magnitude Spectrum)', cv.WINDOW_AUTOSIZE)
+    cv.imshow(f'{fileName} (Magnitude Spectrum)', mag)
+    if(not(magOnly)):
+        pha=np.angle(fft, deg=True)
+        cv.namedWindow(f'{fileName} (Phase Spectrum)', cv.WINDOW_AUTOSIZE)
+        cv.imshow(f'{fileName} (Phase Spectrum)', pha)
+    cv.waitKey(0)
