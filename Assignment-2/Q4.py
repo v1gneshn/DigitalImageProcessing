bb18b2394db4efd84da0c57e428413e2c3b6c8b6
Aditya DS <adithya18062000@gmail.com>
2021-04-10 13:09:13 +0530

    - set magOnly to true - added waitkey

--- Assignment-2/Q4.py
+++ Assignment-2/Q4.py
@@ -1,23 +1,16 @@
 import numpy as np
 import cv2 as cv
 from fft2 import fft2
+from helpers import plotSpectrum
 
 lena = cv.imread('./images/Lena.png', cv.IMREAD_GRAYSCALE)
 
 npFFT2 = np.fft.fft2(lena)
-npFFT2 = np.fft.fftshift(np.absolute(npFFT2))
-npFFT2 = 255*(npFFT2/np.amax(npFFT2))
-npFFT2 = 10*np.log10(1+npFFT2)
-
 myFFT2 = fft2(lena)
-myFFT2 = np.fft.fftshift(np.absolute(myFFT2))
-myFFT2 = 255*(myFFT2/np.amax(myFFT2))
-myFFT2 = 10*np.log10(1+myFFT2)
 
-cv.namedWindow('Numpy FFT on Lena (Magnitude Spectrum)', cv.WINDOW_AUTOSIZE)
-cv.imshow('Numpy FFT on Lena (Magnitude Spectrum)', npFFT2)
-cv.namedWindow('Our FFT implementation on Lena (Magnitude Spectrum)', cv.WINDOW_AUTOSIZE)
-cv.imshow('Our FFT implementation on Lena (Magnitude Spectrum)', myFFT2)
+plotSpectrum(npFFT2, 'Numpy FFT on Lena', True)
+plotSpectrum(myFFT2, 'Our FFT implementation on Lena', True)
+
 cv.waitKey(0)
 
 print(np.allclose(myFFT2, npFFT2))
