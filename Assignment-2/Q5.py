6392e44c7cf03e95f2d79ba8605b52ba3496ba79
Aditya DS <adithya18062000@gmail.com>
2021-04-18 17:59:23 +0530

    Q5 output images

--- Assignment-2/Q5.py
+++ Assignment-2/Q5.py
@@ -11,6 +11,14 @@ dog = cv.imread('./images/dog.tif', cv.IMREAD_GRAYSCALE)
 dog = cv.resize(dog, None, fx=0.5689, fy=0.5689)
 dog = np.pad(dog, [(0, 0), (0, 512-dog.shape[1])], mode='constant')
 
+# with built-in functions
+lena_builtin = np.fft.fft2(lena)
+dog_builtin = np.fft.fft2(dog)
+
+plotSpectrum(lena_builtin, 'Lena - Built in')
+plotSpectrum(dog_builtin, 'Dog - Built in')
+
+# with user-defined functions
 lenaFFT = fft2(lena)
 dogFFT = fft2(dog)
 
